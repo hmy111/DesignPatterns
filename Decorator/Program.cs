379fb20267ea3b9953e5381f2c429e7087cb92b0
黄明洋 <mingyang.huang@ly.com>
2021-11-25 19:25:53 +0800

    first commit

---
+++ Decorator/Program.cs
@@ -0,0 +1,54 @@
+﻿IText text = new Text();
+text = new ColorDecorator(text);
+text = new BlodDecorator(text);
+System.Console.WriteLine(text.Content);
+
+Console.ReadKey();
+
+// IComment
+public interface IText
+{
+    public string Content { get; }
+}
+public class Text : IText
+{
+    public string Content => "text";
+}
+
+public interface IDecorator : IText
+{
+}
+// IS IText
+public abstract class DecoratorBase : IDecorator
+{
+    // HAS IText
+    protected readonly IText _text;
+    public DecoratorBase(IText text)
+    {
+        _text = text;
+    }
+
+    public abstract string Content { get; }
+}
+
+public class ColorDecorator : DecoratorBase
+{
+
+    public ColorDecorator(IText text) : base(text)
+    {
+    }
+
+    public override string Content => $"{base._text.Content} color add red";
+}
+
+public class BlodDecorator : DecoratorBase
+{
+    private readonly IText text;
+
+    public BlodDecorator(IText text) : base(text)
+    {
+        this.text = text;
+    }
+
+    public override string Content => $"{this.text.Content} color add blod";
+}
---
+++ Test/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+sealed class DecoratorAttribute : Attribute
+{
+    public readonly object Injector;
+    private readonly Type _type;
+    public DecoratorAttribute(Type type)
+    {
+        _type = type ?? throw new ArgumentNullException(nameof(type));
+        Injector = new Assembler().Create(_type);
+    }
+    public Type @Type => this._type;
+}
+
+static class AttributeHelper
+{
+    /// <summary>
+    /// target:" 客户端
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public static T Injector<T>(object target) where T : class
+    {
+        if (target == null) throw new ArgumentNullException();
+
+        target.GetType().GetCustomAttributes(typeof(ITimeProvider), false);
+    }
+}
+
+internal class Assembler
+{
+    private static Dictionary<Type, Type> _cache = new Dictionary<Type, Type>();
+
+    static Assembler()
+    {
+        _cache.Add(typeof(ITimeProvider), typeof(TimeProvider));
+    }
+
+    public object Create(Type type)
+    {
+        if (type == null || !_cache.ContainsKey(type)) throw new ArgumentNullException();
+
+        Type traget = _cache[type];
+        return Activator.CreateInstance(traget)!;
+    }
+
+    public T Create<T>()
+    {
+        return (T)Create(typeof(T));
+    }
+}
+
+public interface ITimeProvider
+{
+    DateTime CurrentDate { get; }
+}
+
+public class TimeProvider : ITimeProvider
+{
+    public DateTime CurrentDate => DateTime.Now;
+}
